7b5f140887bfa0b1ed1729ae84d4fc0000d1fa82
Adnan Khattak <adnanktk540@gmail.com>
2024-01-01 11:11:54 +0500

    form validation sucess

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -5,38 +5,37 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
-  ScrollView
+  ScrollView,
 } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
+import {useForm, Controller} from 'react-hook-form';
 import {user, welcome} from '../../style/styles';
 
 const SignUp = ({navigation}) => {
-
   const [pending, setPending] = useState(false);
-  
+
   const {
     control,
     handleSubmit,
-    formState: { errors },
+    formState: {errors},
     watch,
   } = useForm({
     defaultValues: {
-      userName: "",
-      phoneNumber: "",
-      password: "",
-      confirmPassword: "",
+      userName: '',
+      phoneNumber: '',
+      password: '',
+      confirmPassword: '',
     },
-  })
+  });
   const password1 = watch('password', '');
 
-  const onSubmit = (data) => {
+  const onSubmit = data => {
     console.log('Form Data:', data);
     setPending(true);
     setTimeout(() => {
-    setPending(false);
-    navigation.navigate('signin');
-    }, 3000); 
-  }
+      setPending(false);
+      navigation.navigate('signin');
+    }, 3000);
+  };
 
   // const handleSignUp = () => {
   //   // Handle the sign-up logic here
@@ -45,115 +44,116 @@ const SignUp = ({navigation}) => {
 
   return (
     <ScrollView contentContainerStyle={user.container}>
-    <View style={user.innerContainer}>
-    <View style={user.logoContainer}>
+      <View style={user.innerContainer}>
+        <View style={user.logoContainer}>
           {/* <Image source={logo} style={user.logo} /> */}
-       <View style={user.title}>
+          <View style={user.title}>
             <Text style={user.titleText}>آپ کی آواز، ہمارا عزم</Text>
             <Text style={user.titleText}>Your Voice, Our Commitment</Text>
           </View>
+        </View>
+
+        <View style={user.form}>
+          <View style={user.formInput}>
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={user.input}
+                  placeholder="User Name | صارف نام"
+                  placeholderTextColor="#a0aec0"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="userName"
+            />
+            {errors.userName && <Text>This is required.</Text>}
           </View>
 
-     <View style={user.form}>
-        <View style={user.formInput}>
-        <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={user.input}
-              placeholder="User Name | صارف نام"
-              placeholderTextColor="#a0aec0"
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            /> )}
-            name="userName"
-          />
-          {errors.userName && <Text>This is required.</Text>}
-    
+          <View style={user.formInput}>
+            <Controller
+              control={control}
+              rules={{
+                required: 'Phone number is required',
+                pattern: {
+                  value: /^[0-9]{11}$/, // Example pattern for a 10-digit phone number
+                  message: 'Invalid phone number',
+                },
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={user.input}
+                  placeholder="Mobile Number | فون نمبر"
+                  placeholderTextColor="#a0aec0"
+                  keyboardType="numeric"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="phoneNumber"
+            />
+            {errors.phoneNumber && <Text>{errors.phoneNumber.message}</Text>}
           </View>
 
-           <View style={user.formInput}>
-           <Controller
-        control={control}
-        rules={{
-          required: "Phone number is required",
-          pattern: {
-            value: /^[0-9]{11}$/, // Example pattern for a 10-digit phone number
-            message: "Invalid phone number",
-          },
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-             <TextInput
-              style={user.input}
-              placeholder="Mobile Number | فون نمبر"
-              placeholderTextColor="#a0aec0"
-              keyboardType="numeric"
-              onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
+          <View style={user.formInput}>
+            <Controller
+              control={control}
+              rules={{
+                required: 'Password is required',
+                minLength: {
+                  value: 8,
+                  message: 'Password must be at least 8 characters long',
+                },
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={user.input}
+                  placeholder="Create Password | پاس ورڈ بنائیں"
+                  placeholderTextColor="#a0aec0"
+                  secureTextEntry
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="password"
             />
-            )}
-            name="phoneNumber"
-          />
-          {errors.phoneNumber && <Text>{errors.phoneNumber.message}</Text>}
-    
-           </View>
-
-           <View style={user.formInput}>
-           <Controller
-        control={control}
-        rules={{
-          required: "Password is required",
-          minLength: {
-            value: 8,
-            message: "Password must be at least 8 characters long",
-          },
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-             <TextInput
-              style={user.input}
-              placeholder="Create Password | پاس ورڈ بنائیں"
-              placeholderTextColor="#a0aec0"
-              secureTextEntry
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-            )}
-            name="password"
-          />
-          {errors.password && <Text>{errors.password.message}</Text>}
+            {errors.password && <Text>{errors.password.message}</Text>}
             {/* <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={user.iconContainer}>
               <Ionicons name={showPassword ? 'eye' : 'eye-off'} size={20} color="gray" /> 
              </TouchableOpacity>  */}
           </View>
 
           <View style={user.formInput}>
-          <Controller
-        control={control}
-        rules={{
-          required: "Confirm password is required",
-          validate: (value) => value === password1 || "Passwords do not match",
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-             <TextInput
-              style={user.input}
-              placeholder="Confirm Password | تصدیق کریں"
-              placeholderTextColor="#a0aec0"
-              secureTextEntry
-              onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
+            <Controller
+              control={control}
+              rules={{
+                required: 'Confirm password is required',
+                validate: value =>
+                  value === password1 || 'Passwords do not match',
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <TextInput
+                  style={user.input}
+                  placeholder="Confirm Password | تصدیق کریں"
+                  placeholderTextColor="#a0aec0"
+                  secureTextEntry
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="confirmPassword"
             />
+            {errors.confirmPassword && ( <Text>{errors.confirmPassword.message}</Text>
             )}
-            name="confirmPassword"
-          />
-          {errors.confirmPassword && <Text>{errors.confirmPassword.message}</Text>}
-    
+
             {/* <TouchableOpacity onPress={() => setConfirmPassword(!confirmPassword)} style={user.iconContainer}>
                <Ionicons name={confirmPassword ? 'eye' : 'eye-off'} size={20} color="gray" /> 
             </TouchableOpacity>  */}
@@ -161,32 +161,28 @@ const SignUp = ({navigation}) => {
           <TouchableOpacity
             style={user.submitButton}
             onPress={handleSubmit(onSubmit)}
-            disabled={pending}
-          >
+            disabled={pending}>
             <Text style={user.buttonText}>
               {!pending ? 'SIGN UP | سائن اپ' : 'Signing Up...'}
             </Text>
           </TouchableOpacity>
-</View>
-      <View style={user.additionalText}>
+        </View>
+        <View style={user.additionalText}>
           <Text style={user.additionalTextText}>
             Already Registered?{' '}
             <Text
               style={user.clickableText}
-              onPress={() => navigation.navigate('signin')}
-            >
+              onPress={() => navigation.navigate('signin')}>
               Click here
             </Text>
           </Text>
           <Text style={user.additionalTextText}>
-            کیا آپ پہلے ہی رجسٹرڈ ہیں؟
+          ہمارے ساتھ رجسٹر کریں
           </Text>
         </View>
-    </View>
+      </View>
     </ScrollView>
   );
 };
 
 export default SignUp;
-
-
--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, ScrollView,StyleSheet } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
 import { user } from '../../style/styles';
 
 
@@ -8,49 +9,115 @@ const Signin = ({navigation}) => {
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
 
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm({
+    defaultValues: {
+      phoneNumber: "",
+      password: "",
+    },
+  })
+  const onSubmit = (data) => {
+    console.log('Form Data:', data);
+    setPending(true);
+    setTimeout(() => {
+    setPending(false);
+    // navigation.navigate('signin');
+    }, 3000); 
+  }
   const handleSignIn = () => {
     // Add your sign-in logic here
     console.log('Sign In:', { phoneNumber, password });
   };
 
   return (
-    <View style={user.container}>
-      <Text style={user.title}>Sign In</Text>
-
+    <ScrollView contentContainerStyle={user.container}>
+      <View style={user.innerContainer}>
+        <View style={user.logoContainer}>
+          {/* <Image source={logo} style={user.logo} /> */}
+          <View style={user.title}>
+            <Text style={user.titleText}>آپ کی آواز، ہمارا عزم</Text>
+            <Text style={user.titleText}>Your Voice, Our Commitment</Text>
+          </View>
+        </View>
+        <View style={user.form}>
+          <View style={user.formInput}>
+      <Controller
+              control={control}
+              rules={{
+                required: 'Phone number is required',
+                pattern: {
+                  value: /^[0-9]{11}$/, // Example pattern for a 10-digit phone number
+                  message: 'Invalid phone number',
+                },
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
       <TextInput
         style={user.input}
         placeholder="Phone Number"
-        keyboardType="phone-pad"
-        value={phoneNumber}
-        onChangeText={(text) => setPhoneNumber(text)}
+        keyboardType="numeric"
+        onBlur={onBlur}
+        onChangeText={onChange}
+        value={value}
       />
+      )}
+      name="phoneNumber"
+    />
+    {errors.phoneNumber && <Text>{errors.phoneNumber.message}</Text>}
+    </View>
+    
 
-<View style={user.passwordContainer}>
+<View style={user.formInput}>
+<Controller
+        control={control}
+        rules={{
+          required: "Password is required",
+          minLength: {
+            value: 8,
+            message: "Password must be at least 8 characters long",
+          },
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
         <TextInput
           style={user.input}
           placeholder="Password"
           secureTextEntry={!showPassword}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
+          value={value}
+          onBlur={onBlur}
+          onChangeText={onChange}
+        /> )}
+        name="password"
+      />
+      {errors.password && <Text>{errors.password.message}</Text>}
         {/* <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.iconContainer}>
           <Icon name={showPassword ? 'eye' : 'eye-slash'} size={20} color="gray" />
         </TouchableOpacity> */}
       </View>
+      
 
 
-      <TouchableOpacity style={user.button} onPress={handleSignIn}>
-        <Text style={user.ButtonText}>Sign In</Text>
+      <TouchableOpacity style={user.submitButton} onPress={handleSubmit(onSubmit)}>
+        <Text style={user.buttonText}>Sign In</Text>
       </TouchableOpacity>
+      </View>
 
-      <Text style={user.signUpLink}>
+      <View style={user.additionalText}>
+      <Text style={user.additionalTextText}>
           Don't have an account?{' '}
-  <Text style={user.clickHere} onPress={() => navigation.navigate('signup')}>
+  <Text style={user.clickableText} onPress={() => navigation.navigate('signup')}>
     Click here
   </Text>
 </Text>
-
+<Text style={user.additionalTextText}>
+            کیا آپ پہلے ہی رجسٹرڈ ہیں؟
+          </Text>
+</View>
     </View>
+    </ScrollView>
   );
 };
 
